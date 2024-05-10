88b7c38db9a989103bfa94816b2f62663bfac12f
wendtalexander <wendtalexander@protonmail.com>
2024-05-10 11:11:36 +0200

    updating distribution repeats

--- seqanalysis/distributions/distribution_repeats.py
+++ seqanalysis/distributions/distribution_repeats.py
@@ -2,6 +2,7 @@ import re
 import sys
 import glob
 import yaml
+import pathlib
 import numpy as np
 import seqanalysis.util.plot_functions as pf
 import seqanalysis.util.helper_functions as hf
@@ -24,26 +25,39 @@ def get_catch_data(path, intro_notes, bout_chunk):
     list = glob.glob(path)
 
     for i in range(len(list)):
-        with open(list[i] + 'batch.catch', 'r') as file:
+        with open(list[i] + "batch.catch", "r") as file:
             line_list = file.readlines()
-            file_list.extend([list[i] + item.rstrip() + '.not.mat' for item in line_list])
+            file_list.extend(
+                [list[i] + item.rstrip() + ".not.mat" for item in line_list]
+            )
     seqs = hf.get_labels(file_list, intro_notes)
     bouts, _ = hf.get_bouts(seqs, bout_chunk)
 
     return bouts
 
 
-def main(folder_path, folder, syl, labels, bout_chunk):
-    folder_lengths = []
-    for folder_idx in range(len(folder)):
-        bouts = get_data(folder_path+folder[folder_idx]+'*cbin.not.mat', ['E', 'S'], bout_chunk)
+def main(folder_path, syl, labels, bout_chunk):
+    folder_path = pathlib.Path(folder_path)
+    files = list(folder_path.glob("**/[!syll*][!check]*.not.mat"))
 
-        # bouts = get_catch_data(folder_path+folder[folder_idx], ['E', 'S', 'k', 'f'], bout_chunk)
-        all_b = re.findall(syl, bouts)
-        lengths = [len(item) - 2 for item in all_b]
-        folder_lengths.append(lengths)
+    seqs = hf.get_labels(
+        files,
+        [
+            "_",
+            "j",
+            "a",
+            "d",
+            "c",
+            "e",
+        ],
+    )
+    bouts, _ = hf.get_bouts(seqs, bout_chunk)
 
-    max_steps = np.max(np.hstack(folder_lengths))
+    # bouts = get_catch_data(folder_path+folder[folder_idx], ['E', 'S', 'k', 'f'], bout_chunk)
+    all_b = re.findall(syl, bouts)
+    folder_lengths = [len(item) - 0 for item in all_b]
+
+    max_steps = np.max(folder_lengths)
     steps = np.linspace(0, max_steps + 1, max_steps + 2)
 
     fig1, ax1 = plt.subplots(1, 1, figsize=(21 / 2.54, 12 / 2.54), sharex=True)
@@ -51,12 +65,9 @@ def main(folder_path, folder, syl, labels, bout_chunk):
         left=0.13, bottom=0.17, top=0.99, right=0.99, wspace=0.5, hspace=0.7
     )
 
-    for plot_idx in range(len(folder_lengths)):
-        counts, bins = np.histogram(folder_lengths[plot_idx], steps)
+    counts, bins = np.histogram(folder_lengths, steps)
 
-        ax1.plot(
-            bins[:-1], counts / np.sum(counts), label=labels[plot_idx], linewidth=4
-        )
+    ax1.plot(bins[:-1], counts / np.sum(counts), label=labels, linewidth=4)
 
     ax1.set_xlabel("Repeat number", fontsize=25)
     ax1.set_ylabel("Rel. frequency", fontsize=25)
@@ -66,19 +77,10 @@ def main(folder_path, folder, syl, labels, bout_chunk):
     ax1.spines["right"].set_visible(False)
     ax1.tick_params(width=2)
     ax1.tick_params(axis="both", which="major", labelsize=20)
-=======
-        ax1.plot(bins[1:-1], counts[1:]/np.sum(counts[1:]), label=labels[plot_idx], linewidth=4)
+    # ax1.plot(bins[1:-1], counts[1:]/np.sum(counts[1:]), label=labels[plot_idx], linewidth=4)
 
-    ax1.set_xlabel('Repeat number', fontsize=20)
-    ax1.set_ylabel('Rel. frequency', fontsize=20)
-
-    ax1.spines["top"].set_visible(False)
-    ax1.spines["right"].set_visible(False)
-    ax1.tick_params(width=2)
-    ax1.tick_params(axis='both', which='major', labelsize=18)
->>>>>>> main
     plt.legend()
-    fig1.savefig(f'..\\..\\..\\data\\or05pk04\\figures\\{syllable}_dist.svg')
+    fig1.savefig(f"{syl}_repeats.pdf")
 
     plt.show()
 
@@ -92,28 +94,15 @@ if __name__ == "__main__":
     #
     # OUTPUT:
     # figures
-    syllable = "fj?+k+b"
-    path = (
-        "C:/Users/awendt/Documents/Masterthesis/data/ye01pu08_tweetynet/training_data/"
-    )
+    syllable = "l+"
+    path = "/home/acfw/Projects/data/gr88gr06_tweetynet/training_data"
     labels = [f"{syllable}"]
-    bout_chunk = "bchg"
+    bout_chunk = "ll"
 
-    folders = ["C:/Users/awendt/Documents/Masterthesis/transition_matrix/ye01pu08/"]
-    plt.rcParams['svg.fonttype'] = 'none'  # this is so that svg figures save text as text and not the single letters
-    plt.rcParams['font.size'] = 20
-    plt.rcParams['axes.linewidth'] = 2
+    plt.rcParams["svg.fonttype"] = (
+        "none"  # this is so that svg figures save text as text and not the single letters
+    )
+    plt.rcParams["font.size"] = 20
+    plt.rcParams["axes.linewidth"] = 2
 
-    syllable = 'fb+'
-    # syllable = 'db+'
-    # syllable = 'db+(?![w])'
-    path = 'D:/Birds/or05pk04/*/*/'
-    # labels = [f'{syllable}']
-    bout_chunk = 'b'
-
-    # folders = ['22110*/', '22111[^45]/', '221115/', '221116/', '221117/', '221118/', '221121/', '230807/']
-    folders = ['22110*/', '22111[4-5]/', '22111[6-8]/', '221121/', '230807/']
-    labels = ['BL', 'T1/2', 'T3-5', 'post', '11M PE']
->>>>>>> main
-
-    main(path, folders, syllable, labels, bout_chunk)
+    main(path, syllable, labels, bout_chunk)
